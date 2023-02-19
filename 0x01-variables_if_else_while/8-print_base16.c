a386295a51df57e8de02492d09bb5dde01f30910
Program-Tunde <haircartelsaloon@gmail.com>
2023-02-19 11:18:47 -0800

    betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,11 +5,10 @@
  *Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 	int n = '0';
 	int a_to_f = 'a';
-
 
 	while (n <= '9') /*print 0-9*/
 	{
@@ -17,13 +16,11 @@ int main(void)
 		n++;
 	}
 
-
 	while (a_to_f <= 'f') /*print a-f to finish hexbase*/
 	{
 		putchar(a_to_f);
 		a_to_f++;
 	}
-
 
 	putchar('\n');
 

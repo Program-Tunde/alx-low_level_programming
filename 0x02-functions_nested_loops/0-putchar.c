fb0bc0e681986e2966c650cf224524160c5ec578
Program-Tunde <haircartelsaloon@gmail.com>
2023-02-21 15:48:37 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar.
- *
- * Return: always 0 (Success)
-*/
+ * main - print _putchar, followed by new line
+ * Return: 0
+ */
 
 int main(void)
 {
-	int nums[] = {95, 112, 117, 116, 99, 104, 97, 114};
-
-	int index, size;
-
-	size = sizeof(nums) / sizeof(nums[index]);
-
-	for (index = 0; index < size; index++)
-	{
-		_putchar(nums[index]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }

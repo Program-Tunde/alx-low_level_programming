016fc0a778971f7acf8a48a8c76a06443e3ee528
Program-Tunde <haircartelsaloon@gmail.com>
2023-02-21 16:41:17 -0800

    integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: first  argument
+ * @b: second argument
+ * Return: sum of the two intergers 
+ */
+
+int add(int n, int x)
+{
+	return (n + x);
+}
